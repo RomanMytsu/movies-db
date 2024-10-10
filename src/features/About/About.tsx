a2e1d6c129dd0664fbdf1d5be92d2281db6c7ada
RomanMytsu <132835114+RomanMytsu@users.noreply.github.com>
2024-10-10 15:19:35 +0300

    add create portal

--- src/features/About/About.tsx
+++ src/features/About/About.tsx
@@ -1,12 +1,14 @@
 import { Container } from "@mui/material";
 import CountDownText from "./CountDownText";
 import { CountDownVideo } from "./CountDownVideo";
+import MapView from "./MapView";
 
 function About() {
   return (
     <Container sx={{ py: 8 }} maxWidth="md">
       <CountDownText />
       <CountDownVideo />
+      <MapView />
     </Container>
   );
 }
